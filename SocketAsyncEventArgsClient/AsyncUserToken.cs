0a326ce30218ba2ea8b6b8b8b95841bd154afb58
buhuiqizi <buhuiqiziz@outlook.com>
2019-04-23 17:28:37 +0800

    简易地写了发送信息给服务端，接收来自服务端信息的功能。仅仅只是为了测试服务端的功能是否正常。

---
+++ SocketAsyncEventArgsClient/AsyncUserToken.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketAsyncEventArgsOfficeDemo
+{
+
+    //用户类
+
+    class AsyncUserToken
+    {
+        ///<summary>
+        ///客户端IP地址
+        ///</summary>
+        public IPAddress IPAddress { get; set; }
+
+        ///<summary>
+        ///远程地址
+        ///</summary>>
+        public EndPoint Remote { get; set; }
+
+        ///<summary>
+        ///通信SOCKET
+        ///</summary>>
+        public Socket Socket { get; set; }
+
+        ///<summary>
+        ///连接时间
+        ///</summary>
+        public DateTime ConnectTime { get; set; }
+
+        ///<symmary>
+        ///包长
+        ///</symmary>
+        public int packageLen { get; set; }
+
+        ///<symmary>
+        ///包类型
+        ///</symmary>
+        public int packageType { get; set; }
+
+        ///<symmary>
+        ///是否还有数据可以发送
+        ///</symmary>
+        public List<int> sendPacketNum { get; set; }
+
+        ///<summary>
+        ///所属用户信息      要自定义
+        ///</summary>>
+        //public UserInfoModel UserInfo { get; set; }
+
+        ///<summary>
+        ///数据缓存区
+        ///</summary>
+        public List<byte> receiveBuffer { get; set; }
+        public List<byte> sendBuffer { get; set; }
+
+        public AsyncUserToken()
+        {
+            this.receiveBuffer = new List<byte>();
+            this.sendBuffer = new List<byte>();
+            this.sendPacketNum = new List<int>();
+            this.packageLen = 0;
+            this.packageType = 0;
+        }
+    }
+}
---
+++ SocketAsyncEventArgsClient/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SocketAsyncEventArgsClient
+{
+    class Program
+    {
+        Socket socket;
+
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.Listen();
+            program.SendMessage();
+            while (Console.Read() > 0)
+            {
+                program.SendMessage();
+            }
+        }
+
+        private void Listen()
+        {
+            
+            //获取框内Ip地址
+            IPAddress localaddress = IPAddress.Parse("127.0.0.1");
+
+            //创建端点
+            IPEndPoint endPoint = new IPEndPoint(localaddress, int.Parse("5730"));
+
+            //Ip4地址，流方式，Tcp协议
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+
+            //连接端点
+            socket.Connect(endPoint);
+
+            Thread thread = new Thread(() => Receive());
+            thread.Start();
+
+
+            if (socket != null)
+            {
+                Console.WriteLine("连接成功");
+            }
+        }
+
+        private void SendMessage()
+        {
+            string str = "Hello World";
+            int packageType = 1;
+            int packageLen = str.Length + 8;
+            byte[] bType = System.BitConverter.GetBytes(packageType);
+            byte[] bLen = System.BitConverter.GetBytes(packageLen);
+            //将数据放入发送buffer
+            List<byte> allList = new List<byte>();
+            allList.AddRange(bType);
+            allList.AddRange(bLen);
+            allList.AddRange(System.Text.Encoding.Default.GetBytes(str));
+            byte[] sendByte = allList.ToArray();
+
+            socket.Send(sendByte);
+        }
+
+        private void Receive()
+        {
+            Console.WriteLine("开始接收");
+            while (true)
+            {
+                Console.WriteLine("开始展示");
+                Byte[] buffer = new Byte[1024];
+                Byte[] buffer2 = new Byte[1024];
+                int length = socket.Receive(buffer, 1024, SocketFlags.None);
+                Array.Copy(buffer, 8, buffer2, 0, buffer.Count() - 8);
+                String xiaoxi = Encoding.Default.GetString(buffer2);
+                Console.WriteLine(xiaoxi);
+
+            }
+            socket.Close();
+        }
+    }
+
+}
